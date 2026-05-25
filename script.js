e8ee76d623af888da3688598e004f16b67cb86d3
El-Jeffe81 <76759332+El-Jeffe81@users.noreply.github.com>
2026-05-25 14:50:01 -0400

    Refactor version fetching logic in script.js

--- script.js
+++ script.js
@@ -7,20 +7,19 @@ fetch("./tools/nav_bar.html")
   });
 
 
-fetch("./tools/version.html")
-  .then(response => response.text())
-  .then(data => {
-    document.getElementById("version").innerHTML = data;
-  });
-
-
-
 fetch("https://api.github.com/repos/El-Jeffe81/Jeff_Stimple/commits/main")
   .then(res => res.json())
   .then(data => {
     document.getElementById("version").textContent =
       data.sha.substring(0, 7);
-  })
+  });
+
   .catch(() => {
     document.getElementById("version").textContent = "unknown";
+  });
+
+fetch("./tools/version.html")
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById("version").innerHTML = data;
   });
