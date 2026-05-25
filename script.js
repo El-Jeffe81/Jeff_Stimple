c2ce1538cbee682de685d8fff2e1c6752c2e0921
El-Jeffe81 <76759332+El-Jeffe81@users.noreply.github.com>
2026-05-25 15:42:21 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,34 @@
 /* script.js */
 
+/* script.js */
+
+
 // NAVBAR
 fetch("./tools/nav_bar.html")
   .then(response => response.text())
   .then(data => {
+
     document.getElementById("navbar").innerHTML = data;
+
   });
 
 
-// VERSION
-fetch("https://api.github.com/repos/El-Jeffe81/Jeff_Stimple/commits/main")
+
+
+// FOOTER
+fetch("./tools/default_footer.html")
+  .then(response => response.text())
+  .then(data => {
+
+    // inject footer HTML first
+    document.getElementById("default_footer").innerHTML = data;
+
+    // THEN fetch GitHub version
+    return fetch(
+      "https://api.github.com/repos/El-Jeffe81/Jeff_Stimple/commits/main"
+    );
+
+  })
   .then(res => res.json())
   .then(data => {
 
@@ -23,14 +42,3 @@ fetch("https://api.github.com/repos/El-Jeffe81/Jeff_Stimple/commits/main")
       "unknown";
 
   });
-
-
-// FOOTER (disabled for now)
-
-/*
-fetch("./tools/version.html")
-  .then(response => response.text())
-  .then(data => {
-    document.getElementById("default_footer").innerHTML = data;
-  });
-*/
